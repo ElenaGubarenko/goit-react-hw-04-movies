393152b05f7d5d6b52a5eb5b0109ad88dcf41426
Elena Gubarenko <lenagritsishina@gmail.com>
2021-05-09 21:02:42 +0300

    adding changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
-import HomePage from './components/HomePage';
+import { Suspense, lazy } from 'react';
+// import HomePage from './components/HomePage';
 import Header from './components/Header';
-import MoviesPage from './components/MoviesPage';
-import MovieDetailsPage from './components/MovieDetailsPage';
+// import MoviesPage from './components/MoviesPage';
+// import MovieDetailsPage from './components/MovieDetailsPage';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import routes from './routes';
+
+const HomePage = lazy(() =>
+  import('./components/HomePage' /* webpackChunkName: "HomePage" */),
+);
+const MovieDetailsPage = lazy(() =>
+  import(
+    './components/MovieDetailsPage' /* webpackChunkName: "MovieDetailsPage" */
+  ),
+);
+const MoviesPage = lazy(() =>
+  import('./components/MoviesPage' /* webpackChunkName: "MoviesPage" */),
+);
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
-      <Switch>
-        <div className="container">
-          <Route path={routes.homepage} exact component={HomePage} />
-          <Route path={routes.movieDetailsPage} component={MovieDetailsPage} />
-          <Route path={routes.movies} exact component={MoviesPage} />
-          {/* <Route component={HomePage} /> */}
-        </div>
-      </Switch>
+      <Suspense fallback={<h1>Loading...</h1>}>
+        <Switch>
+          <div className="container">
+            <Route path={routes.homepage} exact component={HomePage} />
+            <Route
+              path={routes.movieDetailsPage}
+              component={MovieDetailsPage}
+            />
+            <Route path={routes.movies} exact component={MoviesPage} />
+            {/* <Route component={HomePage} /> */}
+          </div>
+        </Switch>
+      </Suspense>
     </BrowserRouter>
   );
 }
